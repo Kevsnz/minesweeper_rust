ca3e8fd6f1a90d97e4b33005e0e302dc547f4345
Kevsnz <15689588+Kevsnz@users.noreply.github.com>
2022-11-27 12:36:51 +0500

    Field drawing fixes

--- src/draw.rs
+++ src/draw.rs
@@ -201,7 +201,7 @@ impl<'a> Drawer<'a> {
                     (true, true, TileContent::Empty(_)) => Rect::new(36, 36, 16, 16),
                     (true, false, TileContent::Empty(i)) => Rect::new(i * 18, 0, 16, 16),
                     (false, false, TileContent::Bomb) => {
-                        if let GameState::Playing = game.state() {
+                        if matches!(game.state(), GameState::Playing) {
                             Rect::new(0, 36, 16, 16)
                         } else {
                             Rect::new(0, 18, 16, 16)
@@ -215,7 +215,7 @@ impl<'a> Drawer<'a> {
                         }
                     }
 
-                    (_, true, TileContent::Bomb) => Rect::new(18, 36, 16, 16),
+                    (false, true, _) => Rect::new(18, 36, 16, 16),
                     (_, _, _) => Rect::new(36, 18, 16, 16),
                 };
 
